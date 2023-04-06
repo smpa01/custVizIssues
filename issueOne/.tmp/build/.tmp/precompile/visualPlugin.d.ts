798c1a1935fc0a80b510a500645e123d950d6a94
smpa01 <39008835+smpa01@users.noreply.github.com>
2023-04-06 15:21:26 -0400

    issue added

---
+++ issueOne/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var issueOneE6157419E7824215A4DECA700465DD6A_DEBUG: IVisualPlugin;
+export default issueOneE6157419E7824215A4DECA700465DD6A_DEBUG;
